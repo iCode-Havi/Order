2d8fa8f867faefe1bdaf4f174dfa5bd336c60a0e
siriwardhanasmnp <siriwardhanasmnp.19@uom.lk>
2022-11-06 07:52:46 +0530

    Entities

---
+++ src/main/java/com/havi/order/entity/Order.java
@@ -0,0 +1,32 @@
+package com.havi.order.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long orderCode;
+    private Integer quantity;
+    private String customerStatus;
+    private LocalDateTime time;
+    private String address;
+    private Long email;
+    private LocalDateTime customerReceiveTime;
+    private Long payment;
+    private Long contact;
+    private String farmerGivenStatus;
+
+
+}
